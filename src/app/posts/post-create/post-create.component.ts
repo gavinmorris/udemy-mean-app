3e63e65e09132b575edbd704fd90a3c7c3aedd38
Gavin Morris <gavin@dhcp-892b2021.ucd.ie>
2018-09-18 13:17:35 +0100

    two-way binding replaced with posts.service.ts class

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -11,17 +11,17 @@ export class PostCreateComponent {
 
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated =  new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {
+
+  }
 
   onAddPost(form: NgForm) {
 
     if (form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+
+    this.postsService.addPost(form.value.title, form.value.content);
   }
 }
